5e7f4222cd7a738656e80502bdcfdda83686cd44
Bac Kim <backim11022005@gmail.com>
2026-03-27 10:28:42 +0700

    python 2_3

---
+++ lab2_3/hh.py
@@ -0,0 +1,10 @@
+# Buoc 1: Nhap du lieu tu ban phim
+# Ham input() luon tra ve kieu chu (string), nen ta can dung int() de chuyen sang so nguyen
+a = int(input("Nhap so nguyen thu nhat: "))
+b = int(input("Nhap so nguyen thu nhat: "))
+
+# Buoc 2: Tinh tong
+tong = a + b
+
+# Buoc 3: In ket qua ra man hinh
+print("Tong cua hai so la:", tong)
